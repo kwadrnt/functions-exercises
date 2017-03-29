5cbbec10ea011042903f4b6f7a21d099223ea3b2
kwadrnt <kevinctse@gmail.com>
2017-03-29 13:22:10 -0700

    answered first 3 questions

--- functions.js
+++ functions.js
@@ -1,1 +1,31 @@
 // TODO: add your solutions here!
+
+
+// 1. Build your own concatenation
+
+function combineStr(str1, str2) {
+	result = str1 + str2
+	return result
+}
+var result = combineStr('bull', 'dog');
+console.log(result);
+// displays 'bulldog'
+
+// 2. Repeat a phrase
+function repeatPhrase(phrase, n) {
+	for (var i = 0; i < n; i++) {
+		return phrase.repeat(n);
+		console.log(phrase);
+	}
+}
+
+repeatPhrase("Hello", 5);
+
+// 3. Build your own Power Function
+function toTheNthPower(number, power) {
+	result = number**power;	
+	return result;	
+}
+
+var result = toTheNthPower(4, 2);
+console.log(result);
